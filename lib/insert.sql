2db2faf594973e88e5c2c69b5874ccf0bf53206f
Mkikii <maureenwkarimi@gmail.com>
2025-08-31 18:17:31 +0300

    all test passing

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES
+(1, 'Mr.Chocolate', 5, 'M', 'Brown', 'melancholic', 1),
+(2, 'Rowdy', 4, 'M', 'Black', 'choleric', 1),
+(3, 'Tabitha', 3, 'F', 'White', 'melancholic', 1),
+(4, 'Sergeant Brown', 5, 'M', 'Brown', 'melancholic', 1),
+(5, 'Melissa', 5, 'F', 'Brown', 'choleric', 1),
+(6, 'Grinch', 2, 'M', 'Black', 'melancholic', 1),
+(7, 'Wendy', 5, 'F', 'Brown', 'melancholic', 1),
+(8, NULL, 6, 'M', 'Brown', 'choleric', 1);
